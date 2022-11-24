f184ee410cc179ff6019f1f18d82dcae6cb13ac6
Fra-Battaglia <francescobattaglia03@gmail.com>
2022-11-24 17:01:23 +0100

    project setup and script divided into phases

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// 1 - Dare la possibilità di inserire il nome
+// 2 - Dare la possibilità di inserire il cognome
+// 3 - Dare la possibilità di inserire il colore preferito
+// 4 - Concatenare le informazioni e aggiungere a fine stringa il numero 21
+// 5 - Visualizzare il risultato

0968bb4e096c4fcaef44cf2f8e5449197700bc30
Fra-Battaglia <francescobattaglia03@gmail.com>
2022-11-24 17:22:19 +0100

    exercise-done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // 1 - Dare la possibilità di inserire il nome
+
+let first_name = prompt('Inserisci il tuo nome');
+console.log(first_name);
+
 // 2 - Dare la possibilità di inserire il cognome
+
+let last_name = prompt('Inserisci il tuo cognome');
+console.log(last_name);
+
 // 3 - Dare la possibilità di inserire il colore preferito
+
+let favourite_color = prompt('Inserisci il tuo colore preferito');
+console.log(favourite_color);
+
 // 4 - Concatenare le informazioni e aggiungere a fine stringa il numero 21
+
+document.getElementById('your-password').innerHTML = first_name + last_name + favourite_color + '21';
+
 // 5 - Visualizzare il risultato
